e0e72e4082ad16db2a430b8f6def81f6585b3a9d
BuiDucTrung <hohoaianh2107@gmail.com>
2023-10-13 21:07:18 +0700

    add thumbnail open graph

--- app/page.tsx
+++ app/page.tsx
@@ -21,19 +21,6 @@ export const metadata: Metadata = {
     title: "Soulmate",
     description: "Nothing but Moon",
     url: "https://eco-system-sigma.vercel.app/",
-    images: [
-      {
-        url: "https://nextjs.org/og.png",
-        width: 800,
-        height: 600,
-      },
-      {
-        url: "https://nextjs.org/og-alt.png",
-        width: 1800,
-        height: 1600,
-        alt: "My custom alt",
-      },
-    ],
   },
 };
 
